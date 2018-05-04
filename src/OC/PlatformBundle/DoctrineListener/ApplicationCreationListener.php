67bfd0391fef1cb9a3aad768316cb40857cb57fd
anhdao <anhdao.lelievre@gmail.com>
2018-05-04 16:15:12 +0200

    ajout formulaire

--- src/OC/PlatformBundle/DoctrineListener/ApplicationCreationListener.php
+++ src/OC/PlatformBundle/DoctrineListener/ApplicationCreationListener.php
@@ -13,17 +13,21 @@ class ApplicationCreationListener
    * @var ApplicationMailer
    */
   private $applicationMailer;
+
   public function __construct(ApplicationMailer $applicationMailer)
   {
     $this->applicationMailer = $applicationMailer;
   }
+
   public function postPersist(LifecycleEventArgs $args)
   {
     $entity = $args->getObject();
+
     // On ne veut envoyer un email que pour les entités Application
     if (!$entity instanceof Application) {
       return;
     }
+
     $this->applicationMailer->sendNewNotification($entity);
   }
 }
--- src/OC/PlatformBundle/Email/ApplicationMailer.php
+++ src/OC/PlatformBundle/Email/ApplicationMailer.php
@@ -11,20 +11,24 @@ class ApplicationMailer
    * @var \Swift_Mailer
    */
   private $mailer;
+
   public function __construct(\Swift_Mailer $mailer)
   {
     $this->mailer = $mailer;
   }
+
   public function sendNewNotification(Application $application)
   {
     $message = new \Swift_Message(
       'Nouvelle candidature',
       'Vous avez reçu une nouvelle candidature.'
     );
+
     $message
       ->addTo($application->getAdvert()->getAuthor()) // Ici bien sûr il faudrait un attribut "email", j'utilise "author" à la place
       ->addFrom('admin@votresite.com')
     ;
+
     $this->mailer->send($message);
   }
 }
